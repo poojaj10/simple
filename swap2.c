09c93082c0cff14a94fcf98d52e3c5912a528821
poojaj10 <47823218+poojaj10@users.noreply.github.com>
2019-02-22 12:58:58 +0530

    Create swap2.c

---
+++ swap2.c
@@ -0,0 +1,35 @@
+#include <stdio.h> 
+
+int main() 
+
+{ 
+
+  int x, y,t; 
+  int a,b,b;
+  
+
+  printf("Enter two integers\n"); 
+
+  scanf("%d%d", &x, &y, &z); 
+
+  
+
+  printf("Before Swapping\nFirst integer = %d\nSecond integer = %d\n", x, y); 
+
+  
+
+  t = x; 
+
+  x = y; 
+
+  y = t; 
+
+  
+
+  printf("After Swapping\nFirst integer = %d\nSecond integer = %d\n", x, y); 
+
+  
+
+  return 0; 
+
+} 
